40dc46fb89df8a082747438e4df199aa0e793156
MiniaczQ <jakub.motyka.2000@gmail.com>
2021-02-01 17:28:42 +0100

    working settings

--- settings.py
+++ settings.py
@@ -2,46 +2,33 @@
 
 from json import load, dump
 
-__defaults = {
-    'log_path': 'logs'
+settings = {}
+
+defaults = {
+    'cock': 5,
+    'and': False,
+    'balls': "bruh"
+}
+
+validator = {
+    'cock': lambda v: v and type(v) == int,
+    'and': lambda v: v and type(v) == bool,
+    'balls': lambda v: v and type(v) == str
 }
 
 try:
     with open('settings.json', 'r') as __file:
-        __settings = load(__file)
-        __fix_settings = False
-
-        __g = globals()
-
-        for key, value in __defaults.items():
-            if not (__settings.get(key) and type(__settings[key]) == type(__defaults[key])):
-                __settings[key] = __defaults[key]
-                __fix_settings = True
-            __g[key] = __settings[key]
-        
-    if __fix_settings:
-        with open('settings.json', 'w') as __file:
-            dump(__settings, __file, indent=2)
-
+        loaded = load(__file)
+        for key in defaults.keys():
+            l = loaded.get(key)
+            settings[key] = l if validator[key](l) else defaults[key]
 except OSError:
-    #   File not found, use default settings, create a settings file
-
+    #   No file found
+    #   Use defaults
+    settings = defaults
+finally:
     with open('settings.json', 'w') as __file:
-        dump(__defaults, __file, indent=2)
-    pass
-
-
-
-
-
-#   Testing
+        dump(settings, __file, indent=2)
 
 if __name__ == '__main__':
-    print(__settings)
-
-
-setting = (default, validator)
-
-def validate(dictionary, validator):
-    for key, value in dictionary.update(defaults).items():
-        
+    print(settings)

f1c395defca43ac8e9fe40e5d301327a2f26e762
MiniaczQ <jakub.motyka.2000@gmail.com>
2021-02-01 15:49:56 +0100

    working logger

---
+++ inside_settings.py
@@ -0,0 +1,2 @@
+logs_file = 'logs'
+worlds_file = 'worlds'
---
+++ logger.py
@@ -0,0 +1,77 @@
+#   Simplified logging interface
+
+from datetime import datetime
+from os import getcwd, path, makedirs
+from shutil import rmtree
+
+from inside_settings import logs_file
+
+class Logger:
+    def __init__(self, *streams):
+        '''
+        Handles logging.\n
+        *streams - additional streams to write to
+        '''
+        self.logs_path = path.join(getcwd(), logs_path)
+
+        #   Create directory if needed
+        if not path.exists(self.logs_path):
+            try:
+                makedirs(self.logs_path)
+            except OSError:
+                pass
+
+        date_log_path = path.join(self.logs_path, f'{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.txt')
+        date_log_file = open(date_log_path, 'w')
+
+        latest_log_path = path.join(self.logs_path, 'latest.txt')
+        latest_log_file = open(latest_log_path, 'w')
+
+        self.__streams = streams + (date_log_file, latest_log_file)
+    
+    @staticmethod
+    def __finalize(msg):
+        '''
+        Add time and new line to a message.
+        '''
+        return f'[{datetime.now().strftime("%H:%M:%S")}] {msg}\n'
+
+    def __write(self, msg):
+        '''
+        Finalize and send the message to all streams, then flush the streams.
+        '''
+        for stream in self.__streams:
+            stream.write(self.__finalize(msg))
+
+        for stream in self.__streams:
+            stream.flush()
+
+    def info(self, msg):
+        '''
+        Send an information level message.
+        '''
+        self.__write(f'[INFO] {msg}')
+    
+    def warn(self, msg):
+        '''
+        Send a warning level message.
+        '''
+        self.__write(f'[WARN] {msg}')
+
+
+
+
+
+#   Testing
+
+if __name__ == "__main__":
+    from sys import stdout
+    from time import sleep
+
+    logger = Logger(stdout)
+
+    logger.info('Hello')
+    sleep(1)
+    logger.warn('world!')
+    sleep(1)
+    logger.info(':)')

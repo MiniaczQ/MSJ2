09d7104b77719e3ee92daa2c1680472748d0cf79
MiniaczQ <jakub.motyka.2000@gmail.com>
2021-02-06 01:29:12 +0100

    New settings

--- settings.py
+++ settings.py
@@ -1,21 +1,34 @@
 #   settings.json validator
 
 from json import load, dump
+from shlex import split
 
 from logger import logger
 
 settings = {}
 
+#   Dictionary of default values for every setting
 defaults = {
-    'cock': 5,
-    'and': False,
-    'balls': "bruh"
+    'server_jar_name': 'server.jar',
+    'java_arguments': '',
+    'motd': '@name',
+    'max_servers': 5,
+    'server_ip': '127.0.0.1',
+    'visible_port': 25565,
+    'min_local_port': 25566,
+    'nogui': False
 }
 
+#   Unnamed funcitons that return whether a setting is valid
 validator = {
-    'cock': lambda v: v is not None and type(v) == int,
-    'and': lambda v: v is not None and type(v) == bool,
-    'balls': lambda v: v is not None and type(v) == str
+    'server_jar_name': lambda v: v is not None and type(v) == str and v.endswith('.jar'),
+    'java_arguments': lambda v: v is not None and type(v) == str,
+    'motd': lambda v: v is not None and type(v) == str,
+    'max_servers': lambda v: v is not None and type(v) == int and 0 < v and v <= 26,
+    'server_ip': lambda v: v is not None and type(v) == str,
+    'visible_port': lambda v: v is not None and type(v) == int and 0 <= v and v <= 65535,
+    'min_local_port': lambda v: v is not None and type(v) == int and 0 <= v and v <= 65535,
+    'nogui': lambda v: v is not None and type(v) == bool
 }
 
 try:
@@ -29,12 +42,16 @@ try:
                 settings[key] = defaults[key]
                 logger.warn(f'Invalid setting "{key}" with value: {l}')
 except OSError:
-    #   No file found
-    #   Use defaults
     settings = defaults
 finally:
     with open('settings.json', 'w') as __file:
         dump(settings, __file, indent=2)
+
+
+
+
+
+#   Testing
 
 if __name__ == '__main__':
     print(settings)
